8d51e38563c9c59dada106ababa15c269b0bba03
‘Kalophain14’ <chueutemosho@gmail.com>
2026-01-05 19:05:37 +0200

    First Class + Java Skeleton

---
+++ 01 - Introduction/First.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+class First
+{
+    public static void main(String args[]) //method
+    {
+        System.out.println("Java is awesome");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
