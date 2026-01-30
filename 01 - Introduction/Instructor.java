9ffba836b5ec2ab714af9ec428a957aad75abaa9
Kalophain14 <chueutemosho@gmail.com>
2026-01-30 22:22:51 +0200

    Instructor

--- 01 - Introduction/Instructor.java
+++ 01 - Introduction/Instructor.java
@@ -1,4 +1,6 @@
 // First program
+// JVM calls main method which is the function of the class
+// writing static helps make it easier to call it
 
 public class Instructor{
     public static void main(String[] args){
