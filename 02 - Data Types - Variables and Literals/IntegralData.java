2d0ec3dcc81b47125b9b749463cd9cfde4832479
Kalophain14 <chueutemosho@gmail.com>
2026-02-06 19:23:01 +0200

    DataTypes,Binary & ASCII

--- 02 - Data Types - Variables and Literals/IntegralData.java
+++ 02 - Data Types - Variables and Literals/IntegralData.java
@@ -5,7 +5,27 @@ CPU example
 32 bit
 16 bit
 
+Binary
+
+Byte = A number can be stored in 7 bit
+7 6 5 4 3 2 1 0
+0 0 0 0 0 0 0 0 = +
+  1 1 1 1 1 1 1 = Largest value
+One of the bit is reserved as Sign bit and it decide what kind of data is going to be stored -/+,
+0 = +
+1 = -
+
+if the first digit is - and the data stored will be -
 
 
- */
+*/
 
+public class IntegralData {
+    public static void main(String[] args) {
+        //int x = -5;
+        //System.out.println(Integer.toBinaryString(x));
+        for (char c = 0x90; c <= 0x097; c++) {
+            System.out.println(c + " ");
+        }
+    }
+}
