1ba6c2c0a9fe6596fc43d87f9c9801073149dbeb
Kalophain14 <chueutemosho@gmail.com>
2026-02-07 19:15:07 +0200

    Increment ++ Decrement --

---
+++ 05 - Operator and Expressions/IncreDecr.java
@@ -0,0 +1,35 @@
+/*
+increment ++
+decrement --
+
+post incr ++ (x++)
+post decr -- (x--)
+
+++ pre incr (++x)
+-- pre decr (--x)
+
+byte x = 4;
+x++; // increase the value by 1
+sout(x);
+
+float x = 5.5;
+x--; // decrease the value by 1
+sout(x);
+
+int x = 5;
+++x; increse the value by 1 and add it to
+
+char x = 'A';
+--x
+sout(x);
+
+ */
+
+public class IncreDecr
+{
+    public static void main(String[] args)
+    {
+        char a = 'A';
+        System.out.println(a);
+    }
+}
