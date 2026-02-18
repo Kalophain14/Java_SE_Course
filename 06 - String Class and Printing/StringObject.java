b9a495c8290fdc47eacebd87857781464065cb2a
‘Kalophain14’ <chueutemosho@gmail.com>
2026-02-18 17:15:24 +0200

    String constructors in real code

--- 06 - String Class and Printing/StringObject.java
+++ 06 - String Class and Printing/StringObject.java
@@ -30,3 +30,29 @@ once you state new String(java):
 
 If its already in pool it cant be created twice as it already exists, a copy will made referencing it
  */
+
+public class StringObject {
+    public static void main(String[] args) {
+        //String(String)
+        String str1 = "Java Programming";
+        String str2 = new String("java is awesome");
+        System.out.println(str2) ;
+
+        //String(char [])
+        char c[] = {'H', 'E', 'L', 'L', 'O'};
+        String str3 = new String(c);
+        System.out.println(str3);
+
+        //String(byte [])
+        byte b[] = {65, 66, 67, 68};
+        String str4 = new String(b,0,3); //printing in array from index 0 and item 2 ABC
+        System.out.println(str4);
+
+        // Boolean True or False
+        String str5 = "Java Programming";
+        String str6 = new String("Java");
+        System.out.println(str5.equals(str6));
+
+
+    }
+}
