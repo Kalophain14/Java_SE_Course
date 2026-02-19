96b39e94ec87442255b5ad7a405df8141dc1f70c
‘Kalophain14’ <chueutemosho@gmail.com>
2026-02-19 21:30:40 +0200

    String methods all different methods

--- 06 - String Class and Printing/StringMeth.java
+++ 06 - String Class and Printing/StringMeth.java
@@ -1,6 +1,30 @@
 /*
 Part 1
 
+*String Methods*
+toLowerCase() > ABCD == abcd
+toUpperCase() > abcd == ABCD
+trim() > removes the blank spaces in a string (best used when user gives input)
+substring(int begin) > it prints from starting index
+substring(int begin, int end) > it prints from beginning and ending index (3, 6)
+replace(char old, char new) > it replaces/modified the letter chosen to another letter
+valueOf(int i) > 
+
+*boolean Methods*
+startsWith(String s)
+endsWith(String s)
+equals(String s)
+equalsIgnoreCase(String s)
+
+*Character Methods*
+charAt(int index)
+
+*int Methods*
+length()
+indexOf(String s)
+lastIndexOf(String s)
+compareTo(Strings s)
+
 Part 2
 
 Part 3
