5651d578e84a69e91a1831f9e0eff2ff939cd209
‘Kalophain14’ <chueutemosho@gmail.com>
2026-02-20 22:25:00 +0200

    Student challange #2

--- 06 - String Class and Printing/StringCh.java
+++ 06 - String Class and Printing/StringCh.java
@@ -2,21 +2,35 @@
 
 public class StringCh {
     public static void main(String[] args) {
-        String str = "programmer@gmail.com";
 
+        // stringMethods Challange
+        String str = "programmer@gmail.com";
         int i = str.indexOf('@');
-        //int j = str.indexOf(".s");
         String username = str.substring(0, i);
         String domain = str.substring(i+1);
 
         System.out.println("Username: " + username);
         System.out.println("Domain: " + domain);
+
+        //RegularExpressions challange
+        //Binary
+        int b = 100010;
+        String str2 = String.valueOf(b); //convert int to string
+        System.out.println(str2.matches("[01]+"));
+
+        //Hex or decicmal
+        String str3 = "424AD";
+        System.out.println(str3.matches("[0-9A-F]+"));
+
+        //Datesformat dd/mm/yyyy
+        String str4 = "20/02/2026";
+        System.out.println(str4.matches("\\d{2}/\\d{2}/\\d{4}")); //Match exactly 2 digits,then 2 digits,then 4 digits
+
+
     }
 }
 
 //Regular Expression
 
 
-
-//Regular Expression 2
  //System.out.println("is the email a gmail account? " + str.matches(".*[gmail]*."));
