96df0879d355b0e818ceeedf65bdba5ba8be96a0
‘Kalophain14’ <chueutemosho@gmail.com>
2026-02-20 21:48:40 +0200

    Student challange #1

--- 06 - String Class and Printing/StringCh.java
+++ 06 - String Class and Printing/StringCh.java
@@ -1,7 +1,22 @@
-/*
-String Methods
+//String Methods
 
-Regular Expression
+public class StringCh {
+    public static void main(String[] args) {
+        String str = "programmer@gmail.com";
 
-Regular Expression 2
- */
+        int i = str.indexOf('@');
+        //int j = str.indexOf(".s");
+        String username = str.substring(0, i);
+        String domain = str.substring(i+1);
+
+        System.out.println("Username: " + username);
+        System.out.println("Domain: " + domain);
+    }
+}
+
+//Regular Expression
+
+
+
+//Regular Expression 2
+ //System.out.println("is the email a gmail account? " + str.matches(".*[gmail]*."));
