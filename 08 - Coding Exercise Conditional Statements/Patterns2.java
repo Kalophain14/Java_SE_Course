2809888109e0ac535f50d9f3e2fbe1a823d93cd8
Temoso <46405512+Kalophain14@users.noreply.github.com>
2026-03-22 19:47:30 +0200

    Feat: More in patterns understanding nested loops, Diamonds leave forever

--- 08 - Coding Exercise Conditional Statements/Patterns2.java
+++ 08 - Coding Exercise Conditional Statements/Patterns2.java
@@ -28,8 +28,8 @@
  */
 public class Patterns2 {
 
-    public static void main(String[] args){
-        int rows = 5;
+    public static void main(String[] args) {
+        /* int rows = 5;
         int[] triangle = new int[rows]; //arrays stores the num from the rows
 
 
@@ -52,6 +52,43 @@ public class Patterns2 {
                 System.out.print(triangle[j] + "   ");
             }
 
+            System.out.println();
+        }*/
+
+        int n = 5;
+
+        // Top half (including middle)
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                if (j <= n - i)
+                    System.out.print("  ");
+                else
+                    System.out.print("* ");
+            }
+            for (int j = 1; j < i; j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        // Bottom half
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                System.out.print("  ");
+            }
+            for (int j = i; j < n; j++)
+            {
+                System.out.print("* ");
+            }
+            for (int j = 1; j < n - i; j++)
+            {
+                System.out.print("* ");
+            }
             System.out.println();
         }
     }
