1c7551a4e843bff2a0c8605f2cc5039317e31827
Temoso <46405512+Kalophain14@users.noreply.github.com>
2026-03-22 21:44:54 +0200

    Learned: - Initializing arrays in different ways - How to access an array using for loop and for each loop - for each loop easy to access an array only forward direction for(i : num) - For loop used to access forward and backwards in an array

--- 09- Arrays/OneDimensionalArrays.java
+++ 09- Arrays/OneDimensionalArrays.java
@@ -1,6 +1,46 @@
+/*
+
+Arrays
+
+What is an array = a collection of similar data elements int, double, etc
+- You cant store different data types in array for example x = [2, 4, 4, 6, 10]
+- any array has a lenth of an array
+
+ */
+
 public class OneDimensionalArrays {
 
     public static void main(String[] args) {
-        // One-Dimensional Arrays
+
+        // Ways to declare an array
+
+        // reference   object created in heap
+        int num[] = new int[5]; //creating an empty array without knowing the values
+        //storing data in the array per index
+        num[0] = 2;
+        num[1] = 4;
+        num[2] = 6;
+        num[3] = 8;
+        num[4] = 10;
+
+        int numA[] = {2, 4, 6, 8, 10}; //creating an array knowing the values
+
+        int numB[]; //declare a reference
+        numB = new int[10]; //creating an object after
+
+        for(int i = 0; i < num.length; i++) {
+            System.out.println(num[i]++);
+        }
+
+        //using for each loop easiest way to access the array
+        // only in forward direction
+        for(int i: num){
+            System.out.println(i);
+        }
+
+        //reverse
+        for(int i = num.length -1; i>=0; i--){
+            System.out.println(num[i]);
+        }
     }
 }
