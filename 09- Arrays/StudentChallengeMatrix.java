c21243c76b79f8b919b68cf40bae23afe7c5d788
Temoso <46405512+Kalophain14@users.noreply.github.com>
2026-04-16 21:34:54 +0200

    feat: Challanges
    - adding 2 Matrix
    - multiplying 2 matrix

--- 09- Arrays/StudentChallengeMatrix.java
+++ 09- Arrays/StudentChallengeMatrix.java
@@ -3,7 +3,8 @@ public class StudentChallengeMatrix {
     public static void main(String[] args)
     {
 
-        // Adding 2 Matrix
+        System.out.println("===Adding 2D Matrix===");
+
         int A[][] = {{3,5,9},{7,6,2},{4,3,5}};
         int B[][] = {{1,5,2},{6,8,4},{3,9,7}};
 
@@ -16,16 +17,41 @@ public class StudentChallengeMatrix {
                 C[i][j] = A[i][j] + B[i][j];
             }
         }
-        for(int x[]:C)
+        for(int add[]:C)
         {
-            for(int y:x)
+            for(int result:add)
             {
-                System.out.print(y+" ");
+                System.out.print(result+" ");
             }
             System.out.println("");
         }
 
-        //Multiplying 2 Matrix
+        System.out.println("===Multiplying 2D Matrix===");
+
+        int D[][] = {{3,5,9},{7,6,2},{4,3,5}};
+        int E[][] = {{1,0,0},{0,1,0},{0,0,1}};
+
+        int F[][] = new int[3][3];
+
+        for(int i = 0; i< D.length; i++)
+        {
+            for (int j = 0; j < D[0].length; j++)
+            {//Multiplying
+                F[i][j] = D[i][j]*C[i][j];
+            }
+        }
+
+        for(int multi[]: D){
+            for(int result2: multi){
+                System.out.print(result2 + " ");
+            }
+            System.out.println("");
+        }
+
+        System.out.println("===Sorting Array Of Strings===");
+
+
+
 
     }
 }
