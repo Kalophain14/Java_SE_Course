ef20a74bbacca23c0d0111e2615aeeb4b3111b1c
Temoso <chueutemosho@gmail.com>
2026-04-17 22:10:26 +0200

    feat:
    - Understanding how methods are created normally called functions in other languages
    - They are created to help re-use the same code again and again without creating the same code multiple times
    - It uses parameters and has a return type of a datatype
    - Calling method from parameters

--- 10 - Methods/PassingObjectAsParameter.java
+++ 10 - Methods/PassingObjectAsParameter.java
@@ -1,6 +1,38 @@
-public class PassingObjectAsParameter {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
+public class PassingObjectAsParameter
+{
+    //Updating an array method
+    static void update(int A[]) //passing an empty array as a parameter
+    {
+        A[0] = 25;
+    }
+
+    //Username method
+    static String userName(String email) {
+        int a = email.indexOf('@'); //searches for the name before it reaches @
+        String name = email.substring(0, a); //cuts until the @ sign
+        return name;
+    }
+
+    public static void main(String[] args)
+    {
         // Passing Object as Parameter
+        int A[]={2,3,4,5,6};
+        update(A);
+        System.out.println(A[0]);
+
+        //Username
+        System.out.println("===Extracting Username==");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your email: ");
+        String email = scanner.nextLine();
+
+        String result = userName(email);
+
+        System.out.println("Your username is: " + result);
+
+        scanner.close();
     }
 }
