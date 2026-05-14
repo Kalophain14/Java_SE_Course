199f09a8a040e88d83e325efff978209ae40ae6a
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-14 22:27:45 +0200

    Added some error handing for a wrong operator if entered

--- 07-conditional-statements/SwitchCase.java
+++ 07-conditional-statements/SwitchCase.java
@@ -70,7 +70,7 @@ public class SwitchCase {
             case '*' -> result = num1 * num2;
             case '/' -> {
                 if (num2 == 0) {
-                    System.out.println(" \"Invalid number\" ");
+                    System.out.println(" \"Cannot divide by zero 🤢\" ");
                     validOperation = false;
                 }else {
                     result = num1 / num2;
@@ -78,7 +78,7 @@ public class SwitchCase {
             }
             case '^' -> result = Math.pow(num1, num2);
             default -> {
-                System.out.println("Invalid Operator");
+                System.out.println("Invalid operator");
                 validOperation = false;
             }
 
