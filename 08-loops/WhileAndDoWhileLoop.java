1c986fd1f0756b21744d7ebd2fe972490126a497
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-15 19:42:28 +0200

    While and do While Loop the easier to validation examples names, etc

--- 08-loops/WhileAndDoWhileLoop.java
+++ 08-loops/WhileAndDoWhileLoop.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * ============================================================
  *  TOPIC: while Loop  vs  do-while Loop
@@ -36,7 +38,7 @@
 public class WhileAndDoWhileLoop {
 
     public static void main(String[] args) {
-        int i = 1, n = 100;
+        /*int i = 1, n = 100;
         int x = 1, y = 100;
 
         while (i < 100) {
@@ -44,11 +46,64 @@ public class WhileAndDoWhileLoop {
             i = i*2;
         }
 
-
         System.out.println();
         do {
             System.out.println(x);
             x = x*2;
         } while (x < y);
+
+
+
+        //Name Validation
+        Scanner input = new Scanner(System.in);
+        String name = "";
+
+        while (name.isEmpty()) {
+            System.out.print("Enter your Name: ");
+            name = input.nextLine();
+        }
+
+        System.out.println("Your name is " + name);
+        input.close();
+
+        //Game Quit
+        String response = "";
+
+        Scanner in = new Scanner(System.in);
+        while (!response.equals("Q")) {
+            System.out.print("You are playing a game");
+            System.out.println("Press Q to quit: ");
+            response = in.next().toUpperCase(); //handle lower or upper cases
+        }
+
+
+        //Age Validation
+        Scanner scanner = new Scanner(System.in);
+        int age = 0;
+
+        System.out.println("Enter your age: ");
+        age = scanner.nextInt();
+        
+        while (age < 0) {
+            System.out.println("Your age cant be negative");
+            System.out.println("Enter your age: ");
+            age = scanner.nextInt();
+        }
+        System.out.println("You are " + age + " years old");
+        */
+
+        //doWhile Version
+
+        Scanner scanner = new Scanner(System.in);
+        int age = 0;
+
+        do {
+            System.out.println("Your age cant be negative");
+            System.out.println("Enter your age: ");
+            age = scanner.nextInt();
+        } while (age < 0);
+
+        System.out.println("You are " + age + " years old");
+        scanner.close();
     }
 }
