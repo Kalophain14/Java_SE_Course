fb78e25384db718ae22dc289faca85e21b85c487
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-15 20:39:21 +0200

    A game of guessing using do while, random, and scanner

---
+++ 08-loops/GuessGame.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessGame {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        Scanner input = new Scanner(System.in);
+        
+        int guess;
+        int attempts = 0;
+        int randonNumber = rand.nextInt(1, 11);
+
+        System.out.println("==Number Guessing Game==");
+        System.out.println("Guess a number between 1 to 10: ");
+
+        do {
+            System.out.print("Enter a number: ");
+            guess = input.nextInt();
+            attempts++;
+
+        }while(guess != randonNumber);
+
+        System.out.println("Congrats, you have won");
+    }
+}
