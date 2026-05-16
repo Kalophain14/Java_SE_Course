ae315bc68b4a8248d83951f63a46349c2f0c62aa
‘Kalophain14’ <chueutemosho@gmail.com>
2026-05-16 20:49:39 +0200

    For loop count as you know, a new year count down

--- 08-loops/ForLoop.java
+++ 08-loops/ForLoop.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * ============================================================
  *  TOPIC: for Loop
@@ -43,10 +45,19 @@
  * ============================================================
  */
 public class ForLoop {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        for (int i = 10; i > 0; i-=1){
+        System.out.print("How many numbers to countdown: ");
+        int start = scanner.nextInt();
+
+        for(int i = start; i > 0; i-=1){
             System.out.println(i);
+            Thread.sleep(1000);
         }
+
+        System.out.println("Happy New Year");
+
+        scanner.close();
     }
 }
